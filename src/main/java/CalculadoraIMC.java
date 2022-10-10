6303a2ac2a35b95084c395a52925aea58ac7db30
Lfelipe-Freitas <lfelipe.dfreitas@gmail.com>
2022-10-10 14:09:02 -0300

    Exercício de introdução ao Java.

---
+++ src/main/java/CalculadoraIMC.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalculadoraIMC {
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Qual o seu nome?");
+        String user = entrada.nextLine();
+
+        System.out.println("Qual o seu peso?");
+        int peso = entrada.nextInt();
+
+        System.out.println("Qual a sua altura?");
+        double altura = entrada.nextDouble();
+        double imc = peso / (altura * altura); // peso / Math.pow(altura, 2)
+
+        System.out.printf(user + " o seu IMC é = %.2f\n", imc); // %.2f mostra apenas 2 casas decimais.
+        System.out.printf("O seu nome é %s e seu peso é %d", user, peso);
+        // %s é string, %d é inteiros e %f é decimais.
+    }
+}
---
+++ src/main/java/Variaveis.java
@@ -0,0 +1,13 @@
+public class Variaveis {
+    //Execução do nosso programa java
+    public static void main(String[] args) {
+        System.out.println("Here we go again...");
+        int idade = 20; //inteiro, representa números até 2 bilhões
+        double salario = 4500.99; //dupla precisão
+        float nota = 7.5f; //f é sufixo para float
+        long populacaoTerra = 7_900_000_000l; //L maiúsculo ou minúsculo no final do número é o sufixo
+        final String profissao = "Programador"; // final não pode ser alterada, mas pode ser declarada vazia
+        final int destino; //final declarada vazia
+
+    }
+}
